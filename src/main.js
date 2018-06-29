22efb3cc8e464e15a9c12072c84311ba798a4a87
kai.jin <kai.jin@corp.elong.com>
2018-06-29 11:18:11 +0800

    project init

---
+++ src/main.js
@@ -0,0 +1,16 @@
+
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import './assets/style/reset.css'
+import fastClick from 'fastclick'
+fastClick.attach(document.body)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})

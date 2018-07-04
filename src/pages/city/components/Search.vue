fe9d51e2f9a0f828f75dc2134e9c75806681791e
kai.jin <kai.jin@corp.elong.com>
2018-07-04 11:33:58 +0800

    城市搜索框增加动态查询

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,59 @@
 <template>
-  <div class="city-search">
-    <input placeholder="请输入城市" type="text" class="search-input"/>
+  <div>
+    <div class="city-search">
+      <input placeholder="请输入城市或拼音" type="text" class="search-input" v-model="keyword"/>
+    </div>
+    <div class="search-list" ref="searchList" v-show="showResultDiv">
+      <ul>
+        <li v-for="(item ,index) in result" :key="index">{{item}}</li>
+        <li v-show="showResult">没有匹配到数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+  import BScroll from 'better-scroll'
   export default {
-    name: "CitySearch"
+    name: "CitySearch",
+    props: {
+      cities: {
+        type: Object,
+        required: true
+      }
+    },
+    data() {
+      return {
+        keyword: '',
+        searchCitys: [],
+        result: []
+      }
+    },
+    computed:{
+      showResult(){
+        return !this.result.length
+      },
+      showResultDiv(){
+        return this.keyword
+      }
+    },
+    watch: {
+      keyword() {
+        this.result = []
+        if (this.keyword == '')
+          return
+        for (let item in this.cities) {
+          this.cities[item].forEach(city => {
+            if (city.name.indexOf(this.keyword) > -1 || city.spell.indexOf(this.keyword) > -1) {
+              this.result.push(city.name)
+            }
+          })
+        }
+      }
+    },
+    mounted(){
+      this.scroll = new BScroll(this.$refs.searchList)
+    }
   }
 </script>
 
@@ -14,17 +61,32 @@
   @import '~@/assets/style/variables';
 
   .city-search {
-    padding:0.1rem 0.2rem;
+    padding: 0.1rem 0.2rem;
     background-color: $bgColor;
     .search-input {
       box-sizing: border-box;
       border: 1px solid #666;
-      padding:0 0.3rem;
+      padding: 0 0.3rem;
       line-height: 1.5rem;
       border-radius: 0.2rem;
-      width:100%;
-      color:#666;
-      text-align:center;
+      width: 100%;
+      color: #666;
+      text-align: center;
+    }
+  }
+
+  .search-list {
+    z-index: 1;
+    overflow: hidden;
+    background-color: #eee;
+    position: absolute;
+    top: 3.8rem;
+    bottom: 0;
+    width: 100%;
+    padding-left: 0;
+    li {
+      padding: 0.25rem;
+      border-bottom: 1px solid #fff;
     }
   }
 </style>
